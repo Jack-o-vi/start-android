ab8ece39bd243edef0ec41a3d22d67c0a8c3e92d
Vitaly Zeienko <jackovi123@gmail.com>
2018-05-30 23:50:11 +0300

    Create two modules, which demonstrate Tabs.

---
+++ module21_tabintent/src/main/java/com/example/module21_tabintent/OneActivity.java
@@ -0,0 +1,11 @@
+package com.example.module21_tabintent;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OneActivity extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.one);
+    }
+}
